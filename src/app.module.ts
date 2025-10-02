9baeff8729bcabcc48cf3793863932d7cba5c588
christian felipe morales sagredo <christianfelipemoralessagredo@MacBook-Air-de-christian.local>
2025-10-01 21:12:43 -0300

    FIX: fix app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,23 +17,19 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
     ConfigModule.forRoot(),
 
     TypeOrmModule.forRoot({
-      ssl: process.env.STAGE === 'prod'
-        ? { rejectUnauthorized: false }
-        : false,
-      extra: {
-        ssl: process.env.STAGE === 'prod'
-              ? { rejectUnauthorized: false }
-              : null,
-      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
-      database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,      
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, // ⚠️ cuidado en prod, mejor usa migraciones
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
+
 
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'), 
